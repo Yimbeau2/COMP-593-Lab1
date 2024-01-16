51093988b44a23accf7833cb80e5c1cde1ea3ef3
Yimbeau2 <156677831+Yimbeau2@users.noreply.github.com>
2024-01-16 09:01:26 -0500

    MOdified both files in the repo clone
    Deleted a print statement from script1.py

--- Script1.py
+++ Script1.py
@@ -1,4 +1,4 @@
 # Print statments about python libraries
 print(f"Beautiful soup, so brothy and frothy")
 print(f"pandas, I see pandas everywhere")
-print(f" debian flebian ")
+
--- Script2.py
+++ Script2.py
@@ -2,3 +2,4 @@
 print(f"Beautiful is better than ugly")
 print(f"Namespaces are one honking great idea -- let's do more of those!")
 print(f" although never is often better than *righ* now.")
+print(f" I love python")
