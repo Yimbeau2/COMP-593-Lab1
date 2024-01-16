a599c4f109c0747985cb82ddf619574018616be4
Yimbeau2 <156677831+Yimbeau2@users.noreply.github.com>
2024-01-16 09:14:13 -0500

    Added comment to script1.py
    added comment

--- Script1.py
+++ Script1.py
@@ -2,3 +2,4 @@
 print(f"Beautiful soup, so brothy and frothy")
 print(f"pandas, I see pandas everywhere")
 print(f" debian flebian ")
+# Kyle Brophy and Jesse melo are the best CSI teachers at fleming
