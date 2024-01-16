3ac6aa1119b347bd62706cc6fcd3c6b011926a1e
Yimbeau2 <156677831+Yimbeau2@users.noreply.github.com>
2024-01-16 08:51:00 -0500

    Revert "modified both script files"
    This reverts commit b7766016d287b7ef0cf845d743ef0c8a7e78db80.

--- Script2.py
+++ Script2.py
@@ -1,4 +1,3 @@
-#Prints lines about some Legendary Python Libraries
 print(f"Beautiful is better than ugly")
 print(f"Namespaces are one honking great idea -- let's do more of those!")
 print(f" although never is often better than *righ* now.")
