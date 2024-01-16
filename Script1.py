b7766016d287b7ef0cf845d743ef0c8a7e78db80
Yimbeau2 <156677831+Yimbeau2@users.noreply.github.com>
2024-01-16 08:50:14 -0500

    modified both script files
    deleted print from script1.py
    added comment to script2.py

--- Script1.py
+++ Script1.py
@@ -1,4 +1,4 @@
 # Prints lines fro mZen of Python
 print(f"Beautiful soup, so brothy and frothy")
 print(f"pandas, I see pandas everywhere")
-print(f" debian flebian ")
+
--- Script2.py
+++ Script2.py
@@ -1,3 +1,4 @@
+#Prints lines about some Legendary Python Libraries
 print(f"Beautiful is better than ugly")
 print(f"Namespaces are one honking great idea -- let's do more of those!")
 print(f" although never is often better than *righ* now.")
